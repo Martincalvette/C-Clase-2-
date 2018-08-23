f70e0080300c3d8979f6aea7d745b62aeb2b9f9c
alumno <alumno@lab6pc0>
2018-08-23 20:59:09 -0300

    primera version del programa

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //int numero[10];
+    int numero;
+    int numeroMin;
+    int numeroMax;
+    float i;
+    int promedio;
+    int acumNumero;
+
+    i=0;
+
+
+    for(i=0;i<10;i++)
+    {
+    printf("ingrese el numero:", i+1);
+    scanf("%d",&numero);
+       if (i==0)
+       {
+            numeroMax=numero;
+            numeroMin=numero;
+       }
+       else if(numero<numeroMin)
+       {
+           numeroMin=numero;
+       }
+
+       else if(numero>numeroMax)
+       {
+            numeroMax=numero;
+       }
+
+        acumNumero=acumNumero+numero;
+
+    }
+
+        printf("esg", acumNumero);
+        //printf("numero maximo %d\nnumero minimo %d\nel promedio es", numeroMax, numeroMin,promedio);
+
+
+
+    return 0;
+}
+
